5ffb0684df285122f156fd82327ac2a86f284ba9
finnahern <finna9x@gmail.com>
2021-07-03 14:29:41 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+function runGame(){
+
+}
+
+function checkAnswer(){
+
+}
+
+function calculateCorrectAnswer(){
+
+}
+
+function incrementScore(){
+
+}
+
+function incrementWrongAnswer(){
+
+}
+
+function displayAdditionQuestion(){
+
+}
+
+function displaySuntractQuestion(){
+    
+}
+
+function displayMultiplyQuestion(){
+    
+}
+
+function displayDivisionQuestion(){
+    
+}
