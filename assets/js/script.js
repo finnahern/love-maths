78cfae9f3f62701688930e14266197043def7314
finnahern <finna9x@gmail.com>
2021-07-03 19:02:33 +0000

    Update runGame function to remove previous answer after the subit button is clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,6 +22,8 @@ document.addEventListener("DOMContentLoaded", function(){
  * and after the user's answer has been processed
  */
 function runGame(gameType){
+
+    document.getElementById("answer-box").value = "";
     // Create 2 random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
